fe562994b38434da1337544ddd38be3513e79493
Samir <samirjafari2001@icloud.com>
2026-06-03 15:51:32 +0300

    First commit

---
+++ eslint.config.js
@@ -0,0 +1,3 @@
+import baseConfig from '@ssheverev/eslint-config';
+
+export default [...baseConfig];
---
+++ src/app/providers/index.tsx
@@ -0,0 +1,5 @@
+import {FC, PropsWithChildren} from "react";
+
+export const AppProviders: FC<PropsWithChildren> = ({children}) => {
+    return <>{children}</>
+}
